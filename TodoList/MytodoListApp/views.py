b616e5dc07098fcb8e9d84117cf090a05cf4cf26
chitrakakkar <chitrakakkar@yahoo.co.in>
2016-11-14 15:50:05 -0600

    Documentation done.

--- TodoList/MytodoListApp/views.py
+++ TodoList/MytodoListApp/views.py
@@ -8,15 +8,12 @@ from .models import todo
 def index(request):
     # The `POST` has the data from the HTML form that was submitted.
     # ORM queries the database for all of the to-do entries.
-
     if request.method == 'POST':
         # Get the data from the form
         name = request.POST.get('name')
         description = request.POST.get('description')
-        # if (str(name).strip(" ") != "Item name" or str(name).strip(" ") != "") and  (str(description).strip(" ") != "Description" or str(description).strip(" ") !=""):
         # add the data to the database
         items = todo.objects.create(task_name=name, task_description=description)
-
     # Gets the todos we need from the database
     items = todo.objects.all()
     # render the page with the todos list
@@ -24,14 +21,16 @@ def index(request):
 
 
 def delete_todo(request):
+    # a list for id's for the check -boxes
     id =[]
     if request.method == 'POST':
+        # getlist gets you the list of the Ids.
         id =request.POST.getlist("itemsCheckedForDelete")
         # delete an object and send a confirmation response
+    # a loop that goes through all the ids and delete it
     for things in id:
         todo.objects.get(id=things).delete()
 
     items = todo.objects.all()
-    return render(request, 'MytodoListApp/index.html', {'items': items})
-    # return HttpResponseRedirect("/")
-    # return render_to_response('MytodoListApp/index.html', {'items': items})
+    return render(request, 'MytodoListApp/index.html', {'items': items})  # brings back to the same page
+
